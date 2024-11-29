1e36496b061afcdd29cac546fedf4169a2248be7
Tsageon <sagaetshepo@gmail.com>
2024-11-29 11:25:51 +0200

    ada

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require ('express');
+const app = express() ;
+const cors = require('cors');
+const dbRoutes =  require("./routes/db") ;
+const authRoutes = require("./routes/auth")
+
+app.use(cors({
+    origin: '*',  
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials: true
+}));
+
+
+app.use(express.json()) ;
+app.use('/api' , dbRoutes)
+app.use('/api', authRoutes)                
+app.listen(5000,  ()=>{
+    console.log('Server is running on port http://localhost:5000')
+
+})
