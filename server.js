6fbdc3a87300b89437d27264b52e4754f8eafc38
Tsageon <sagaetshepo@gmail.com>
2024-12-03 10:42:29 +0200

    AddedAmessagethatdisplaysallrouteswhenyoujustentertheurlinsteadofcannotetc.

--- server.js
+++ server.js
@@ -14,6 +14,21 @@ app.use(express.json()) ;
 app.use('/api' , dbRoutes)
 app.use('/api', authRoutes)  
 
+app.all('/', (req, res) => {
+    res.status(200).json({
+        message: 'Welcome to the API! Here are the available routes since your new:',
+        routes: {
+            '/api/SignUp': 'POST - Sign Up a new user',
+            '/api/Login': 'POST - Login to your account',
+            '/api/resetPassword': 'POST - Reset your password',
+            '/api/addEmployee': 'POST - Add a new employee',
+            '/api/deleteEmployee/:id': 'DELETE - Delete an employee by ID',
+            '/api/getEmployee': 'GET - Get list of all employees',
+            '/api/updateEmployee/:id': 'PUT - Update employee details by ID'
+        }
+    });
+});
+
 
 app.listen(5000,  ()=>{
     console.log('Server is running on port http://localhost:5000')
